751c4480ddff845c4638cdc9e01bb626e44829a8
‘Charles <charlesthompson.2693@gmail.com>
2020-09-29 02:18:44 +0000

    Refacotored and coded for short and long URl

--- express_server.js
+++ express_server.js
@@ -1,26 +1,66 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; 
 
-app.set("view engine", "ejs");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+const generateRandomString = () => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  while (randomString.length < 6) {
+    randomString += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return randomString;
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+app.get('/urls.json', (req, res) => {
+  res.send(urlDatabase);
+});
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
-
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+  res.send('Ok');
+})
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
+app.get('/urls/:shortUrl', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+    res.redirect(urldatabase[req.params.shortURL]);
+  }else {
+    res.statusCode = 404;
+    res.send('<h3> 404 Aint find it </h3>')
+  }
+});
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}`);
 });

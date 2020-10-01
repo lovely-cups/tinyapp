54664bdf2b6ea7c93487b609cfb321ba59de1a9e
‘Charles <charlesthompson.2693@gmail.com>
2020-10-01 01:46:18 +0000

    removed old post logout info

--- express_server.js
+++ express_server.js
@@ -113,9 +113,8 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect('/urls');
-  if (emailFinder(req.body.email, users)) {
+  const user = emailFinder(req.body.email, users)
+  if (user) {
     if(req.body.password === user.password){
       res.cookie('user_id", user.userID');
       res.redirect('/urls');
